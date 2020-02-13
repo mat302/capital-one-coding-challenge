01841c87c40a40cfefb5d7f01bce289fdfb1dbfc
Mathieu Godin <matgodin@videotron.ca>
2020-02-13 05:45:56 -0500

    in progress

--- count-comments.py
+++ count-comments.py
@@ -41,7 +41,81 @@ languages_extensions = {
     "scpt": "AppleScript",
     "scptd": "AppleScript",
     "applescript": "AppleScript",
-    "sbl": "BASIC"}
+    "sbl": "BASIC",
+    "bas": "QBASIC",
+    "vb": "Visual Basic",
+    "vbs": "VBScript",
+    "vbe": "VBScript",
+    "wsf": "VBScript",
+    "wsc": "VBScript",
+    "c": "C",
+    "h": "C",
+    "cc": "C",
+    "cpp": "C",
+    "cxx": "C",
+    "c++": "C",
+    "hh": "C",
+    "hpp": "C",
+    "hxx": "C",
+    "h++": "C",
+    "cs": "C",
+    "m": "C",
+    "mm": "C",
+    "go": "C",
+    "groovy": "C",
+    "gvy": "C",
+    "gy": "C",
+    "gsh": "C",
+    "cfm": "ColdFusion",
+    "cfc": "ColdFusion",
+    "f77": "Fortran 77",
+    "f": "Fortran 77",
+    "for": "Fortran 77",
+    "fpp": "Fortran 77",
+    "ftn": "Fortran 77",
+    "f90": "Fortran",
+    "f95": "Fortran",
+    "f03": "Fortran",
+    "f08": "Fortran",
+    "hs": "Haskell",
+    "lhs": "Haskell",
+    "tex": "LaTeX",
+    "java": "Java",
+    "js": "JavaScript",
+    "mjs": "JavaScript",
+    "ts": "JavaScript",
+    "jsx": "JavaScript",
+    "tsx": "JavaScript",
+    "sh": "Shell script",
+    "r": "R",
+    "lua": "Lua",
+    "e": "Eiffel",
+    "sql": "SQL",
+    "vhdl": "VHDL",
+    "vhd": "VHDL",
+    "ml": "OCaml",
+    "mli": "OCaml",
+    "pp": "Pascal",
+    "pas": "Pascal",
+    "inc": "Pascal",
+    "pl": "Perl",
+    "p6": "Raku",
+    "raku": "Raku",
+    "php": "PHP",
+    "phtml": "PHP",
+    "php3": "PHP",
+    "php4": "PHP",
+    "php5": "PHP",
+    "php7": "PHP",
+    "ps1": "PowerShell",
+    "py": "Python",
+    "rb": "Ruby",
+    "swift": "Swift",
+    "xml": "XML",
+    "htm": "XML",
+    "html": "XML",
+    "css": "CSS"
+    }
 
 """Dictionary that maps programming languages to their
 respective programming languages single line and multi-line
@@ -55,4 +129,83 @@ languages_comment_tokens = {
     "AppleScript": CommentTokens(
         [SingleLineCommentToken("--")],
         [MultiLineCommentTokens("(*", "*)")]),
-    "BASIC": CommentTokens("REM ")}
+    "BASIC": CommentTokens(
+        [SingleLineCommentToken("REM ")]),
+    "QBASIC": CommentTokens(
+        [SingleLineCommentToken("REM "),
+         SingleLineCommentToken("\'")]),
+    "Visual Basic": CommentTokens(
+        [SingleLineCommentToken("REM "),
+         SingleLineCommentToken("\'")]),
+    "C": CommentTokens(
+        [SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "ColdFusion": CommentTokens(
+        multi=[MultiLineCommentTokens("<!---", "--->")]),
+    "Fortran 77": CommentTokens(
+        [SingleLineCommentToken("\nC"),
+         SingleLineCommentToken("\nc"),
+         SingleLineCommentToken("\n*")]),
+    "Fortran": CommentTokens(
+        [SingleLineCommentToken("!")]),
+    "Haskell": CommentTokens(
+        [SingleLineCommentToken("--")],
+        [MultiLineCommentTokens("{-", "-}")]),
+    "LaTeX": CommentTokens(
+        [SingleLineCommentToken("%")]),
+    "Java": CommentTokens(
+        [SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "JavaScript": CommentTokens(
+        [SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "Lua": CommentTokens(
+        [SingleLineCommentToken("--")],
+        [MultiLineCommentTokens("--[[", "]]")]),
+    "Eiffel": CommentTokens(
+        [SingleLineCommentToken("--")]),
+    "SQL": CommentTokens(
+        [SingleLineCommentToken("--")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "VHDL": CommentTokens(
+        [SingleLineCommentToken("--")]),
+    "OCaml": CommentTokens(
+        multi=[MultiLineCommentTokens("(*", "*)")]),
+    "Pascal": CommentTokens(
+        [SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("{", "}"),
+         MultiLineCommentTokens("(*", "*)")]),
+    "Perl": CommentTokens(
+        [SingleLineCommentToken("#")],
+        [MultiLineCommentTokens("=", "\n=cut\n")]),
+    "Raku": CommentTokens(
+        [SingleLineCommentToken("#")],
+        [MultiLineCommentTokens("=", "\n=cut\n"),
+         MultiLineCommentTokens("#`{", "}"),
+         MultiLineCommentTokens("#`(", ")")]),
+    "PHP": CommentTokens(
+        [SingleLineCommentToken("#"),
+         SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "PowerShell": CommentTokens(
+        [SingleLineCommentToken("#")],
+        [MultiLineCommentTokens("<#", "#>")]),
+    "Python": CommentTokens(
+        [SingleLineCommentToken("#")],
+        [MultiLineCommentTokens("\"\"\"", "\"\"\""),
+         MultiLineCommentTokens("\'\'\'", "\'\'\'")]),
+    "Ruby": CommentTokens(
+        [SingleLineCommentToken("#")],
+        [MultiLineCommentTokens("=begin", "=end")]),
+    "Swift": CommentTokens(
+        [SingleLineCommentToken("//")],
+        [MultiLineCommentTokens("/*", "*/")]),
+    "XML": CommentTokens(
+        multi=[MultiLineCommentTokens("<!--", "-->")]),
+    "CSS": CommentTokens(
+        multi=[MultiLineCommentTokens("/*", "*/")]),
+    "Shell script": CommentTokens(
+        [SingleLineCommentToken("#")]),
+    "R": CommentTokens(
+        [SingleLineCommentToken("#")])
+    }
